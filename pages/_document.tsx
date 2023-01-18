973270d5c0918412f1134c442b4b8397caf47d33
sungman5 <sungman5@gmail.com>
2023-01-18 22:27:16 +0900

    보잘것 없는 리팩토링

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,12 +3,7 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
     return (
         <Html lang="en">
-            <Head>                
-                {/* <link
-                    rel="stylesheet"
-                    as="style"                    
-                    href="https://cdn.jsdelivr.net/gh/orioncactus/pretendard@v1.3.6/dist/web/variable/pretendardvariable.css"
-                /> */}
+            <Head>
                 <link
                     href="https://cdn.jsdelivr.net/gh/sunn-us/SUIT/fonts/static/woff2/SUIT.css"
                     rel="stylesheet"
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,142 +1,50 @@
-import { getPostsData } from "../lib/getPostsData";
+import { getBullshittingsData, getStudiesData } from "../lib/getPostsData";
 import Link from "next/link";
 import { useRouter } from "next/router";
 
-const worksPostPath = "posts/works";
-const devPostPath = "posts/dev";
-const otherPostPath = "posts/other";
-
-export default function Home({
-    lastestWorksPosts,
-    lastestDevPosts,
-    lastestOtherPosts,
-}: any) {
+export default function Home({ allPosts }: any) {
     const router = useRouter();
     return (
         <>
             <section>
-                <div className="articles">
-                    <div>
-                        <h1 className="cat-name">Dev.</h1>
-                        {lastestDevPosts.length === 0 ? (
-                            <p className="no-have-post">
-                                작성된 포스트가 없습니다. 🥺
-                            </p>
-                        ) : (
-                            lastestDevPosts.map((devPost: any) => {
-                                return (
-                                    <>
-                                        <Link
-                                            href={`/dev/${devPost.id}`}
-                                            key={devPost.id}
-                                        >
-                                            <h2>{devPost.title}</h2>
-                                            <p>{devPost.excerpt}</p>
-                                        </Link>
-                                    </>
-                                );
-                            })
-                        )}
-                    </div>
-
-                    <Link href="/dev">
-                        <span className="read-more">전체보기</span>
-                    </Link>
-                </div>
-
-                <div className="articles">
-                    <div>
-                        <h1 className="cat-name">Works</h1>
-                        {lastestWorksPosts.length === 0 ? (
-                            <p className="no-have-post">
-                                작성된 포스트가 없습니다. 🤣
-                            </p>
-                        ) : (
-                            lastestWorksPosts.map((workPost: any) => {
-                                return (
-                                    <>
-                                        <Link
-                                            href={`/works/${workPost.id}`}
-                                            key={workPost.id}
-                                        >
-                                            <h2>{workPost.title}</h2>
-                                            <p>{workPost.excerpt}</p>
-                                        </Link>
-                                    </>
-                                );
-                            })
-                        )}
-                    </div>
-
-                    <Link href="/works">
-                        <span className="read-more">전체보기</span>
-                    </Link>
-                </div>
-
-                <div className="articles">
-                    <div>
-                        <h1 className="cat-name">Other</h1>
-                        {lastestOtherPosts.length === 0 ? (
-                            <p className="no-have-post">
-                                작성된 포스트가 없습니다. 😎
-                            </p>
-                        ) : (
-                            lastestOtherPosts.map((otherPost: any) => {
-                                return (
-                                    <>
-                                        <Link
-                                            href={`/other/${otherPost.id}`}
-                                            key={otherPost.id}
-                                        >
-                                            <h2>{otherPost.title}</h2>
-                                            <p>{otherPost.excerpt}</p>
-                                        </Link>
-                                    </>
-                                );
-                            })
-                        )}
-                    </div>
-                    <Link href="/other">
-                        <span className="read-more">전체보기</span>
-                    </Link>
-                </div>
+                {allPosts.map((post: any) => {
+                    console.log("포스트", post);
+                    return (
+                        <>
+                            <div className="articles">
+                                <Link
+                                    href={`/${post.category}/${post.id}`}
+                                    key={post.id}
+                                >
+                                    <h2>{post.title}</h2>
+                                    <p className="excerpt">{post.excerpt}</p>
+                                    <p className="date">{post.date}</p>                                   
+                                </Link>
+                            </div>
+                        </>
+                    );
+                })}
             </section>
             <style jsx>
                 {`
-                    h1 {
-                        font-size: 3.2rem;
-                        font-weight: 200;
-                        margin-bottom: 3.2rem;
-                    }
-                    div {
-                        margin-bottom: 4rem;
-                    }
                     h2 {
-                        font-size: 1.7rem;
-                        font-weight: 500;
-                        margin-bottom: 0.4rem;
+                        font-size: 1.8rem;
+                        font-weight: 600;
                     }
-                    a {
+                    .articles {
                         margin-bottom: 2.4rem;
+                    }                   
+                    .date {
+                        font-family: "Fira Code", monospace;
+                        font-size: 1.4rem;
                     }
-                    a:hover {
-                        color: red;
+                    .category {
+                        color: rgb(48, 115, 179);
                     }
-                    p,
-                    span {
-                        font-size: 1.67rem;
-                        line-height: 2.4rem;
-                        color: #323232;
-                    }
-                    .read-more {
-                        text-decoration: underline;
-                    }
-                    ul {
-                        margin-bottom: 2rem;
-                    }
-                    .no-have-post {
-                        color: #a1a1a1;
-                        margin-bottom: 1.6rem;
+                    .excerpt {
+                        font-size: 1.5rem;
+                        font-weight: 400;
+                        color: #6e7781;
                     }
                 `}
             </style>
@@ -145,15 +53,22 @@ export default function Home({
 }
 
 export async function getStaticProps() {
-    const lastestWorksPosts = getPostsData(worksPostPath);
-    const lastestDevPosts = getPostsData(devPostPath);
-    const lastestOtherPosts = getPostsData(otherPostPath);
+    const studiesPosts = getStudiesData();
+    const bullshittingsPosts = getBullshittingsData();
+
+    const allPosts = [...studiesPosts, ...bullshittingsPosts];
+
+    console.log("올포스츠", allPosts);
+
+    const sortAllPosts = allPosts.map((post) => {
+        post;
+
+        return {};
+    });
 
     return {
         props: {
-            lastestWorksPosts,
-            lastestDevPosts,
-            lastestOtherPosts,
+            allPosts,
         },
     };
 }
