094b677a887977e3b39015daabeb802f39cf2a6e
sungman5 <sungman5@gmail.com>
2023-01-18 22:27:49 +0900

    deploy to gh-pages

--- _next/static/4Uhd4hL7YeL8KDEM5aWZs/_ssgManifest.js
+++
@@ -1,1 +0,0 @@
-self.__SSG_MANIFEST=new Set(["\u002F","\u002Fdev","\u002Fdev\u002F[id]","\u002Fother","\u002Fother\u002F[id]","\u002Fworks","\u002Fworks\u002F[id]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
