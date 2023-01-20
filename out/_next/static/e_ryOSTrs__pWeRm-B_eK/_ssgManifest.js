4b8d8f20834d1b130dfa2c2e48e69b7529570d79
sungman5 <sungman5@gmail.com>
2023-01-20 23:49:26 +0900

    deploy to gh-pages

--- out/_next/static/e_ryOSTrs__pWeRm-B_eK/_ssgManifest.js
+++
@@ -1,1 +0,0 @@
-self.__SSG_MANIFEST=new Set(["\u002F","\u002Fbullshittings","\u002Fbullshittings\u002F[id]","\u002Fstudies","\u002Fstudies\u002F[id]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
